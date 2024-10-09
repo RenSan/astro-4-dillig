8aa6e87933f0e12771a3a9c1c7f4b1437f01fbee
__renesan <ren.san.r@gmail.com>
2024-10-08 22:18:31 -0600

    feat: add packagecard

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,19 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1e73be',
+        'primary-dark': '#165690',
+        secondary: '#ff6600',
+        background: '#f4f4f4',
+        text: '#333',
+      },
+      transitionProperty: {
+        'height': 'height',
+        'spacing': 'margin, padding',
+      }
+    },
   },
   plugins: [typography],
 };
